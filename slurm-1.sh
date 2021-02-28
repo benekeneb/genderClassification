2d8722767bea4327f8f0d33e5df5ad9fb5397f4e
Klingebiel <ga53kol@TUEINAN-LOGIN1.nano.ei.tum.de>
2021-02-28 20:01:23 +0100

    First Commit Server

---
+++ slurm-1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --mem=40G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=gpucloud
+#SBATCH --gpus=2
+
+module purge
+module load spack_skylake_avx512
+module load python/3.8-cuda-ml
+
+# srun --mpi=pmi2 --pty --mem=40G -n1 --cpus-per-task=8 -p gpucloud --gpus=2 bash
+
+srun python -c "print('THIS WORKS!!!')"
+
+srun python train.py
